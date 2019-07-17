cdb9a3f8e2ab073a83cfb39da03d4eede7abacb4
Anisha Shaji <753943@student.tor.loc>
2019-07-17 16:10:13 -0400

    it is working

--- shoppinglist.js
+++ shoppinglist.js
@@ -3,4 +3,12 @@ const url = require('url');
 const path = reuire('path');
 const{app, BrowserWindow} = electron;
 
+let mainWindow;
+app.on('ready', function(){
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'mainWindow.html'),
+protocol:'file:' ,
+slashes:true
+   })) ;
+})
 
